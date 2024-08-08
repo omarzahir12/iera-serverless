6d2332ed6e054c910899436752e813e2c1d10077
Al-Mahmud <ibnmasud@gmail.com>
2024-08-08 11:21:11 -0400

    notifs

--- src/teams/updateTeam.js
+++ src/teams/updateTeam.js
@@ -1,28 +1,27 @@
-const Joi = require('joi')
-const {insert, find, collections, deleteTempPassword, update} = require('../common/mongo')
-const lambdaReponse = require('../common/lambdaResponse').lambdaReponse
-const COLLECTION = mongodb.collections
-const bcrypt = require('bcryptjs');
-const Boom = require('boom')
-const cryptoRandomString = require('crypto-random-string');
-const setTempPassword = require('../common/setTempPassword').setTempPassword
-const { v4: uuidv4, v5: uuidv5 } = require('uuid');
-const createTeamValidator = {
-    name: Joi.string().required(),
-    description:Joi.string().required(),
-    status:Joi.string().valid('active', 'inactive').default('active')
-}
+const Joi = require("joi");
+const {
+  insert,
+  find,
+  collections,
+  deleteTempPassword,
+  update,
+} = require("../common/mongo");
+const lambdaReponse = require("../common/lambdaResponse").lambdaReponse;
+const Boom = require("boom");
+const createTeamValidator = Joi.object({
+  name: Joi.string().required(),
+  parent_team: Joi.string().required(),
+  status: Joi.string().valid("approved", "enrolled").default("approved"),
+});
 module.exports.handler = async (event) => {
-    const teamId = event.pathParameters.team_id
-    const body = JSON.parse(event.body)
-    const result = Joi.validate(body, createTeamValidator)
-    if (typeof obj !== 'object' || result.error) {
-        return lambdaReponse(Boom.badRequest(result.error))
-    }
-    const team = result.value
-    await update(collections.teams, {_id:teamId}, team)
+  const teamId = event.pathParameters.team_id;
+  const body = JSON.parse(event.body);
+  const result = createTeamValidator.validate(body);
+  if (result.error) {
+    return lambdaReponse(Boom.badRequest(result.error));
+  }
+  const team = result.value;
+  await update(collections.teams, { _id: teamId }, team);
 
-    return lambdaReponse({_id:teamId, ...team})
-    
-  };
-  
+  return lambdaReponse({ _id: teamId, ...team });
+};
